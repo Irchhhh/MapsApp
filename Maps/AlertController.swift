9bf0fea7494cb48615160868ec43835049adedf0
Irina Bex <92269031+Irchhhh@users.noreply.github.com>
2022-04-27 14:21:31 +0300

    Интерфейс

--- Maps/AlertController.swift
+++ Maps/AlertController.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import UIKit
+
+extension MapViewController {
+    
+    func alertAddAddress(title: String, placeholder: String, completionHandler: @escaping (String) -> Void) {
+        
+        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = placeholder
+        }
+        
+        let alertOkAction = UIAlertAction(title: "OK", style: .default) { okAction in
+            let textFieldText = alertController.textFields?.first
+            guard let text = textFieldText?.text else { return }
+            completionHandler(text)
+        }
+        
+        let alertCancelAction = UIAlertAction(title: "Cancel", style: .cancel) { cancelAction in
+            print(cancelAction)
+        }
+        
+        alertController.addAction(alertOkAction)
+        alertController.addAction(alertCancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func errorAlert(title: String, message: String) {
+        
+        let alertErrorController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okErrorAction = UIAlertAction(title: "OK", style: .default)
+        
+        alertErrorController.addAction(okErrorAction)
+        present(alertErrorController, animated: true, completion: nil)
+    }
+}
--- Maps/MapViewController.swift
+++ Maps/MapViewController.swift
@@ -6,24 +6,114 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
+    
+    let mapView: MKMapView  = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return mapView
+    }()
+    
+    let addAddress: UIButton = {
+        let addressButton = UIButton()
+        addressButton.setTitle("ADD ADRESS", for: .normal)
+        addressButton.backgroundColor = .red.withAlphaComponent(0.5)
+        addressButton.layer.cornerRadius = 10
+        addressButton.titleLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: 20)
+        addressButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        return addressButton
+    }()
+    
+    let resetButton: UIButton = {
+        let resetButton = UIButton()
+        resetButton.setTitle("RESET", for: .normal)
+        resetButton.backgroundColor = .red.withAlphaComponent(0.5)
+        resetButton.layer.cornerRadius = 10
+        resetButton.titleLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: 20)
 
+        resetButton.translatesAutoresizingMaskIntoConstraints = false
+        resetButton.isHidden = true
+        
+        return resetButton
+    }()
+    
+    let routeButton: UIButton = {
+        let routeButton = UIButton()
+        routeButton.setTitle("ROUTE", for: .normal)
+        routeButton.backgroundColor = .red.withAlphaComponent(0.5)
+        routeButton.layer.cornerRadius = 10
+        routeButton.titleLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: 20)
+
+        routeButton.translatesAutoresizingMaskIntoConstraints = false
+        routeButton.isHidden = true
+        
+        return routeButton
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setConstraints()
+        
+        addAddress.addTarget(self, action: #selector(addAddressButtonTapped), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(routeButtonTapped), for: .touchUpInside)
+        setConstrainsButtons()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func addAddressButtonTapped() {
+        alertAddAddress(title: "Add Address", placeholder: "Please enter address") { text in
+            print(text)
+        }
     }
-    */
+    
+    @objc func resetButtonTapped() {
+        print("reset")
+    }
+    
+    @objc func routeButtonTapped() {
+        print("route")
+    }
+}
 
+extension MapViewController {
+    
+    func setConstraints() {
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+    
+    func setConstrainsButtons() {
+        mapView.addSubview(addAddress)
+        NSLayoutConstraint.activate([
+            addAddress.topAnchor.constraint(equalTo: mapView.topAnchor, constant: 50),
+            addAddress.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            addAddress.widthAnchor.constraint(equalToConstant: 150)
+        ])
+        
+        mapView.addSubview(routeButton)
+        NSLayoutConstraint.activate([
+            routeButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            routeButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            routeButton.heightAnchor.constraint(equalToConstant: 50),
+            routeButton.widthAnchor.constraint(equalToConstant: 120)
+        ])
+        
+        mapView.addSubview(resetButton)
+        NSLayoutConstraint.activate([
+            resetButton.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: -20),
+            resetButton.bottomAnchor.constraint(equalTo: mapView.bottomAnchor, constant: -30),
+            resetButton.heightAnchor.constraint(equalToConstant: 50),
+            resetButton.widthAnchor.constraint(equalToConstant: 120)
+        ])
+    }
 }
